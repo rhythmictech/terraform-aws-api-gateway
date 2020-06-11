3f2eff995ace84fb1b6226bf9c9587a6051c8fa2
Steven Black <genseb7@gmail.com>
2020-06-11 19:44:24 -0400

    remove deployment and stage from module

--- main.tf
+++ main.tf
@@ -2,25 +2,6 @@
 # API Gateway
 ########################################
 
-resource "aws_api_gateway_base_path_mapping" "this" {
-  domain_name = aws_api_gateway_domain_name.this.domain_name
-  api_id      = aws_api_gateway_rest_api.this.id
-  stage_name  = aws_api_gateway_stage.this.stage_name
-  # TODO: use base_path for API versioning :D
-}
-
-resource "aws_api_gateway_deployment" "this" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  stage_name  = var.stage_name
-  # Error: Unsupported argument
-  # An argument named "triggers" is not expected here.
-  # triggers    = var.triggers
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_api_gateway_domain_name" "this" {
   domain_name              = var.domain_name
   regional_certificate_arn = var.regional_certificate_arn
@@ -46,21 +27,6 @@ resource "aws_api_gateway_rest_api" "this" {
 
   depends_on = [
     aws_api_gateway_rest_api.this
-  ]
-}
-
-resource "aws_api_gateway_stage" "this" {
-  stage_name    = var.stage_name
-  deployment_id = aws_api_gateway_deployment.this.id
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.this.arn
-    format          = var.access_log_format
-  }
-
-  depends_on = [
-    aws_api_gateway_deployment.this
   ]
 }
 
--- variables.tf
+++ variables.tf
@@ -1,10 +1,4 @@
 
-variable "access_log_format" {
-  default     = "$context.identity.sourceIp $context.identity.caller $context.identity.user [$context.requestTime] $context.httpMethod $context.resourcePath $context.protocol $context.status $context.responseLength $context.requestId"
-  description = "The formatting and values recorded in the logs. For more information on configuring the log format rules visit the AWS documentation"
-  type        = string
-}
-
 variable "api_key_source" {
   default     = "HEADER" #tfsec:ignore:GEN001
   description = "The source of the API key for requests. Valid values are HEADER (default) and AUTHORIZER."
@@ -29,33 +23,33 @@ The source of the identity in an incoming request.
 For REQUEST type, this may be a comma-separated list of values, including headers, query string parameters and stage variables - e.g.
 `"method.request.header.SomeHeaderName,method.request.querystring.SomeQueryStringName,stageVariables.SomeStageVariableName"`
 EOT
-  type = string
+  type        = string
 }
 
 variable "log_retention_in_days" {
-  default = 30
+  default     = 30
   description = "Days to retain apigateway logs"
-  type = number
+  type        = number
 }
 
 variable "minimum_compression_size" {
-  default = -1
+  default     = -1
   description = "Minimum response size to compress for the REST API. Integer between -1 and 10485760 (10MB). Setting a value greater than -1 will enable compression, -1 disables compression (default)."
-  type = number
+  type        = number
 }
 
 variable "name" {
   description = "Moniker to apply to all resources in the module"
-  type = string
+  type        = string
 }
 
 variable "provider_arns" {
-  default = []
+  default     = []
   description = <<EOT
 A list of the Amazon Cognito user pool ARNs. Each element is of this format:
 `arn:aws:cognito-idp:{region}:{account_id}:userpool/{user_pool_id}`.
 EOT
-  type = list(string)
+  type        = list(string)
 }
 
 variable "regional_certificate_arn" {
@@ -69,34 +63,20 @@ variable "security_policy" {
   type        = string
 }
 
-variable "stage_name" {
-  description = "The name of the stage"
-  type        = string
-}
-
 variable "tags" {
   default     = {}
   description = "User-Defined tags"
   type        = map(string)
 }
 
-variable "triggers" {
-  default     = {}
-  description = <<EOT
-A map of arbitrary keys and values that, when changed, will trigger a redeployment.
-To force a redeployment without changing these keys/values, use the terraform taint command.
-EOT
-  type = map(string)
-}
-
 variable "types" {
-  type = list(string)
-  default = ["EDGE"]
+  default     = ["EDGE"]
   description = <<EOT
 A list of endpoint types. This resource currently only supports managing a single value.
 Valid values: EDGE, REGIONAL or PRIVATE. If unspecified, defaults to EDGE. Must be declared as REGIONAL in non-Commercial partitions.
 Refer to the documentation for more information on the difference between edge-optimized and regional APIs.
 EOT
+  type        = list(string)
 }
 
 locals {
