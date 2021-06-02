e7b12eec74daf92afb4f9cb7c4311f381d29188d
Steven Black <genseb7@gmail.com>
2021-06-02 18:33:01 -0400

    add hello world api

--- main.tf
+++ main.tf
@@ -30,6 +30,45 @@ resource "aws_api_gateway_rest_api" "this" {
   ]
 }
 
+resource "aws_api_gateway_integration" "hello_world" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.hello_world.id
+  http_method = aws_api_gateway_method.hello_world.http_method
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_deployment" "production" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  stage_name  = "production"
+
+  depends_on = [
+    "aws_api_gateway_integration.hello_world",
+  ]
+}
+
+resource "aws_api_gateway_base_path_mapping" "path_mapping" {
+  base_path = "v1"
+
+  api_id      = aws_api_gateway_rest_api.this.id
+  domain_name = aws_api_gateway_domain_name.this.domain_name
+  stage_name  = aws_api_gateway_deployment.production.stage_name
+}
+
+# /hello_world endpoint
+resource "aws_api_gateway_resource" "hello_world" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "hello_world"
+}
+
+resource "aws_api_gateway_method" "hello_world" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.hello_world.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = try(aws_api_gateway_authorizer.this[0].id, null)
+}
+
 ########################################
 # Authorizer
 ########################################
